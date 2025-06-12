4413e8b733fee04639d9e92e000464fea636adf3
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2025-06-12 20:31:29 -0300

    update

--- ex13.py
+++ ex13.py
@@ -1,2 +1,8 @@
 # Será que é bissexto? Entre com um ano qualquer e verifique se ele é bissexto ou não com seu programa
 
+ano = int(input("Digite um ano: "))
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f"O ano {ano} é Bissexto!")
+else:
+    print(f"O ano {ano} Não é Bissexto")
--- ex14.py
+++ ex14.py
@@ -1,1 +1,12 @@
 # Dados os 3 lados de um triângulo, faça um código para calcular a área deste triângulo
+
+lado1 = int(input("Digite o 1º lado do triângulo: "))
+lado2 = int(input("Digite o 2º lado do triângulo: "))
+lado3 = int(input("Digite o 3º lado do triângulo: "))
+
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
+    s = (lado1 + lado2 + lado3) / 2
+    area = (s * (s-lado1) * (s-lado2) * (s-lado3)) ** (1/2)
+    print(f"A área do triângulo é: {area:.3f}")
+else:
+    print("Não")
